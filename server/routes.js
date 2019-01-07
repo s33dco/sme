5dde4886db043f9dca22a92342fcbbb16933df26
s33dco <code@s33d.co>
2019-01-07 14:04:12 +0000

    add /DELETE/users

--- server/routes.js
+++ server/routes.js
@@ -187,9 +187,6 @@ router.get('/invoices/new', (req, res) => {
 });
 
 router.post('/invoices', validate.invoice, (req, res) => {
-
-  console.log(req.body)
-
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
@@ -777,32 +774,30 @@ router.get('/users/new', (req, res) => {
 });
 
 router.post('/users', validate.user, (req, res) => {
-        // console.log(req.body)
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-          return res.render('users/newuser', {
-            data            : req.body,
-            errors          : errors.mapped(),
-            csrfToken       : req.csrfToken(),  // generate new csrf token
-            pageTitle       : "Add a user",
-            pageDescription : "Give it another shot."
-          });
-        };
+  const errors = validationResult(req)
 
-      const { firstName, lastName, email, mobile, password } = req.body;
-      let user = new User({ firstName, lastName, email, mobile, password });
-
-// todo
-// do not make token and auth new user.
-
-      user.save().then(() => {
-        return user.generateAuthToken();
-      }).then((token) => {
-        req.flash('success', `${user.firstName} ${user.lastName} created !`)
-        res.header('x-auth', token).redirect('/dashboard') // create custom header 'x-auth' with value of token
-      }).catch((e) => {
-        res.status(400).send(e);
+  if (!errors.isEmpty()) {
+    return res.render('users/newuser', {
+      data            : req.body,
+      errors          : errors.mapped(),
+      csrfToken       : req.csrfToken(),  // generate new csrf token
+      pageTitle       : "Add a user",
+      pageDescription : "Give it another shot."
     });
+  };
+
+  const { firstName, lastName, email, password } = req.body;
+  let user = new User({ firstName, lastName, email, password });
+
+    // TODO: why auth token this should be on login only
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    req.flash('success', `${user.firstName} ${user.lastName} created !`)
+    res.header('x-auth', token).redirect('/dashboard') // create custom header 'x-auth' with value of token
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 router.get('/users/:id', (req, res) => {
@@ -827,6 +822,7 @@ router.get('/users/:id', (req, res) => {
     res.render('users/user', {
         pageTitle       : "Users",
         pageDescription : "People with access.",
+        csrfToken: req.csrfToken(),
         user
     });
   }).catch((e) => {
@@ -838,13 +834,132 @@ router.get('/users/:id', (req, res) => {
   });
 });
 
-// PATCH/users/:id
+router.post('/users/edit',  validate.user, (req, res) => {
 
-// DELETE/users/:id  (can't delete yourself)
+  if (!ObjectID.isValid(req.body.id)) {
+    req.flash('alert', "Not possible invalid ID, this may update.");
+    return res.render('404', {
+        pageTitle       : "404",
+        pageDescription : "Invalid resource",
+    });
+  }
+
+  User.findOne({_id: req.body.id})
+  .then((user) => {
+    if (!user ) {
+      req.flash('alert', "Can't find that user...");
+      return res.render('404', {
+          pageTitle       : "404",
+          pageDescription : "Can't find that user"
+      });
+    }
+
+    let { _id, firstName, lastName, email} = user;
+
+    res.render('users/edituser', {
+      data: { _id, firstName, lastName, email },
+      errors: {},
+      csrfToken: req.csrfToken(),
+      pageTitle       : "Edit user",
+      pageDescription : "edit user."
+    })
+  }).catch((e) => {
+    req.flash('alert', `${e.message}`);
+    res.render('404', {
+        pageTitle       : "404",
+        pageDescription : "Invalid resource",
+    });
+  });
+});
+
+// TODO: saving passwords when editing user
+
+router.patch('/users/:id', validate.user ,(req, res) => {
+
+  if (!ObjectID.isValid(req.params.id)) {
+    req.flash('alert', "Not possible invalid ID, this may update.");
+    return res.render('404', {
+        pageTitle       : "404",
+        pageDescription : "Invalid resource",
+    });
+  }
+
+  console.log(req.body)
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.render('users/edituser', {
+        data            : req.body,
+        errors          : errors.mapped(),
+        csrfToken       : req.csrfToken(),
+        pageTitle       : "Edit User",
+        pageDescription : "Give it another shot.",
+    });
+  } else {
+    User.findOne({_id : req.params.id})
+    .then((user) => {
+      return user.updateOne({
+        $set:
+         {
+            firstName : req.body.firstName,
+            lastName  : req.body.lastName,
+            email     : req.body.email,
+            password  : req.body.password
+          }
+        })
+      })
+
+// TODO: something about passwords........
+
+      .then((user) => {
+        req.flash('success', `${req.body.firstName} ${req.body.lastName} updated!`);
+        res.redirect(`/users`);
+      })
+      .catch((e) => {
+        req.flash('alert', `${e.message}`);
+        res.render('404', {
+          pageTitle       : "404",
+          pageDescription : "Invalid resource",
+        });
+      });
+  }
+});
+
+router.delete('/users', (req, res) => {
+
+console.log(req.body)
+
+  const { id, name, billed } = req.body;
+
+  if (!ObjectID.isValid(id)) {
+    req.flash('alert', "Not possible invalid ID, this may update.");
+    return res.render('404', {
+        pageTitle       : "404",
+        pageDescription : "Invalid resource",
+    });
+  }
+
+  User.deleteOne({ _id : id })
+  .then((user) => {
+     req.flash('success', `${req.body.firstName} ${req.body.lastName} deleted!`);
+     res.redirect("/dashboard");
+   }).catch((e) => {
+    req.flash('alert', `${e.message}`);
+    res.render('404', {
+        pageTitle       : "404",
+        pageDescription : "Invalid resource",
+    });
+  });
+});
+//
+
 
 
 // ********************************************
 // detail routes
 // ********************************************
 
+
+// full set of detail routes........
 module.exports = router
