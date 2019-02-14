9070c1c788b3aac33471e3ec81be9a2dffa8fa7f
s33dco <code@s33d.co>
2019-02-14 12:15:39 +0000

    issue with jest and set-cookie headers

--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -3,8 +3,9 @@ const {User}    = require('../../server/models/user');
 const mongoose  = require('mongoose');
 const cheerio   = require('cheerio');
 const app       = require('../../app');
+const agent     = request.agent(app);
 
-let server, user, cookie, csrfToken, password;
+let user, csrfToken, password, cookies;
 
 beforeEach( async () => {
   user = await new User({
@@ -33,7 +34,6 @@ describe('/login', () => {
 
     it('should return the login form', async () => {
       const res = await exec();
-      cookie = res.headers['set-cookie'];
       expect(res.status).toBe(200);
       expect(res.text).toMatch(/Sign In/);
     });
@@ -41,50 +41,41 @@ describe('/login', () => {
 
   describe('POST /', () => {
 
-    const getLogin = async () => {
+    const getLoginCsrfs = async () => {
       const res = await request(app).get(`/login`);
       let $ = cheerio.load(res.text);
       csrfToken = $('[name=_csrf]').val();
-      cookie = res.headers['set-cookie'];
+      cookies = res.headers['set-cookie'][0].split(/,(?=\S)/).map((item) => item.split(';')[0]);
       return res;
     };
 
     const postLogin = async () => {
-      return request(app)
-      .post(`/login`)
-      .set('Cookie', cookie)
-      .send({ email: user.email,
-              password: password,
-              _csrf: csrfToken
-            });
+      return request(app).post(`/login`)
+        .set('Cookie', cookies)
+        .send({ email: user.email,
+                password: password,
+                _csrf: csrfToken
+        });
     };
 
     it('should return 401 without incorrect user info', async () => {
-      await getLogin();
+      await getLoginCsrfs();
       password = 'wrongpassword';
-      // just for sanity...
-      console.log('csrfToken from GET/login form :', csrfToken);
-      console.log('set-cookie from GET/login res.headers :', cookie);
       const res = await postLogin();
       expect(res.status).toBe(401)
     });
 
     it('should return 403 without csrf token/header credentials', async () => {
-      await getLogin();
+      await getLoginCsrfs();
       csrfToken = '';
-      cookie = '';
-      // just for sanity...
-      console.log('csrfToken from GET/login form :', csrfToken);
-      console.log('set-cookie from GET/login res.headers :', cookie);
+      cookies = '';
+      password = 'password';
       const res = await postLogin();
       expect(res.status).toBe(403)
     });
 
     it('should return 200 with correct credentials', async () => {
-      await getLogin();
-      // just for sanity...
-      console.log('csrfToken from GET/login form :', csrfToken);
-      console.log('set-cookie from GET/login res.headers', cookie);
+      await getLoginCsrfs();
       password = 'password';
       const res = await postLogin();
       expect(res.status).toBe(200)
