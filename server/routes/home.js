5db2d635000cf6f7e95845f5fb751e502af19d45
s33dco <code@s33d.co>
2019-03-18 11:49:15 +0000

    fix typo in config

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,9 +1,10 @@
-const express           = require('express');
-const router            = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const config    = require('config');
 
 router.get('/', (req, res) => {
   res.render('index', {
-    pageTitle: "Welcome to SME",
+    pageTitle: `${config.get('SME_TITLE')}`,
     pageDescription: "Static website with invoicing backend."
   })
 });
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -28,7 +28,6 @@ const logger          = require('./logger');
 
 module.exports = (app) => {
 
-  app.locals.email  = config.get('SME_EMAIL');
   app.locals.moment = require('moment');
   app.set('views', path.join(__dirname, '../../views'))
   app.set('view engine', 'ejs')
